7eaaec82eb08dc938a53598cf3cea35f22e13e0a
psgpyc <psgpyc@gmail.com>
2025-03-15 09:43:19 +0000

    feat(models): Added Orders models

--- FindTables/bookings/models.py
+++ FindTables/bookings/models.py
@@ -102,3 +102,50 @@ class WebhookEvent(models.Model):
     
     def __str__(self):
         return f"{self.event_type} - {self.reservation}"
+    
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    course = models.CharField(
+        max_length=50, 
+        choices=[('started', 'Started'),('mains', 'Main Course'),('dessert', 'Dessert'), ('sides', 'Sides')]
+    )
+    is_available = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.name}-{self.price}"
+    
+
+    
+class Orders(models.Model):
+    menu_items = models.ManyToManyField(MenuItem, through='OrderItem')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
+
+    def calculate_total_price(self):
+        total = 0
+        for order_item in self.orderitem_set.select_related('menu_item').all():
+            total += order_item.menu_item.price * order_item.quantity
+        self.total_price = total
+        self.save(update_fields=['total_price'])
+
+
+    def __str__(self):
+        return f"Order {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE, )
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name}"
